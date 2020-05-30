0aea79cce7e722a7a2c97ddc6d1ac851ec94c86b
TunaCOKLU <63820143+TunaCOKLU@users.noreply.github.com>
2020-05-31 01:27:17 +0300

    Add files via upload

---
+++ HW07/src/Canine.java
@@ -0,0 +1,16 @@
+interface Pet {}
+public class Canine implements Pet{
+	
+	
+	public class getDoggy() {
+		return this;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		
+
+	}
+
+}
